b7d7079eff41e344899b30b3fd1512d16674d605
Sanjarbek <sanjarbeksaminjonovv@gmail.com>
2021-04-07 15:19:51 +0500

    commit3

--- translate/urls.py
+++ translate/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainapp.urls')), 
+
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
