9bbeda7d917c0e7cee8f4225c987916e34846c63
SanjarbekSaminjov <sanjarbeksaminjonovv@gmail.com>
2021-09-13 21:01:48 +0500

    changed readme

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # translate
+
 Online tarjimon
 https://dict-uz.herokuapp.com/
 
-It was built on Django 
+It was built on Django
+
+! [Preview] (https://github.com/SanjarbekSaminjonov/translate/blob/main/image.png)
--- translate/settings.py
+++ translate/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
-import dj_database_url
+# import django_heroku
+# import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zji9@j+=lz+^9#cs3rz-p0&sleg^r#89!^lt-5-ja#gv4@p*8('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
